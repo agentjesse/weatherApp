a83a28bfa512b7c2ab33b56d7524b0fdc7d2a7de
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-12 12:40:53 -0400

    xhr request

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var switchDeg = false;
 //starting call
 window.addEventListener('DOMContentLoaded', init);
 
+
+
+
+
 function init(){
   //get current position from navigator object, follow documentation to use it.
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -27,29 +31,50 @@ function init(){
 
 //function that gets and sets weather info from API
 function getWeather(lat,lon){
-  //ajax request
-  $.ajax({
-    //settings
-    // url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
-    url: 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon,
-    type: 'GET',
-    dataType: 'json',
-    //if request is successful then:
-    success(response){
+  //updated to use old xhr method, maybe fix ios problem
+  const xhr = new XMLHttpRequest();
+  const url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon;
+  xhr.responseType='json';
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState === XMLHttpRequest.DONE){
+      //console.log(xhr.response);
       if (switchDeg){
-        $('#weatherContent').html('It\'s ' +Math.round(response.main.temp*1.8+32)+ ' degrees fahrenheit');
+        $('#weatherContent').html('It\'s ' +Math.round(xhr.response.main.temp*1.8+32)+ ' degrees fahrenheit');
         // $('#weatherHold').css({'background-color':'red'});
         // document.getElementById('weatherHold').style.backgroundColor = 'red';
       } else {
-        $('#weatherContent').html('It\'s ' +Math.round(response.main.temp)+ ' degrees celsius');
+        $('#weatherContent').html('It\'s ' +Math.round(xhr.response.main.temp)+ ' degrees celsius');
       }
-      //currenty working on here
-      $('#imageHold').css({'background':"no-repeat center/100% url(" + response.weather[0].icon + ")"});
-    },
-    //if the request failed, then:
-    error(jqXHR,status,errorThrown){
-      console.log( jqXHR );
+      //image
+      $('#imageHold').css({'background':"no-repeat center/100% url(" + xhr.response.weather[0].icon + ")"});
     }
+  };
+  xhr.open('GET',url);
+  xhr.send();
 
-  });
+  //original ajax request fails ios 10.2
+  // $.ajax({
+  //   //settings
+  //   // url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
+  //   url: 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon,
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   //if request is successful then:
+  //   success(response){
+  //     if (switchDeg){
+  //       $('#weatherContent').html('It\'s ' +Math.round(response.main.temp*1.8+32)+ ' degrees fahrenheit');
+  //       // $('#weatherHold').css({'background-color':'red'});
+  //       // document.getElementById('weatherHold').style.backgroundColor = 'red';
+  //     } else {
+  //       $('#weatherContent').html('It\'s ' +Math.round(response.main.temp)+ ' degrees celsius');
+  //     }
+  //     //currenty working on here
+  //     $('#imageHold').css({'background':"no-repeat center/100% url(" + response.weather[0].icon + ")"});
+  //   },
+  //   //if the request failed, then:
+  //   error(jqXHR,status,errorThrown){
+  //     console.log( jqXHR );
+  //   }
+  //
+  // });
 };
